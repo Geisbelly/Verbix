29e0028cadb2e82cec7d4bc120f0e8da7faa17ea
Geisbelly <geisbelly19@gmail.com>
2024-10-21 20:06:49 -0300

    Atualização

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("8e7d8187e3cd3e67")
+/******/ 		__webpack_require__.h = () => ("33d86ab92de3ae8f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
